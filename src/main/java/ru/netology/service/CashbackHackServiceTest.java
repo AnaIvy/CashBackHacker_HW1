e4126ef355798ae947369f8f5725fee7a21c701a
Papillon <anil_061293@gmail.com>
2023-04-29 12:22:26 +0300

    AddTestNG

---
+++ src/main/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,30 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void testRemainIfAmountBelowBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 550;
+        int actual = service.remain(450);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainIfAmountEqualsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int actual = service.remain(1000);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainIfAmountOverBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int actual = service.remain(1500);
+        Assert.assertEquals(actual, expected);
+    }
+}
